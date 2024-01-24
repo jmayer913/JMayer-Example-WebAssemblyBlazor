de071ed7450f2c40e27bdc08617285dc08380505
jmayer913 <72579603+jmayer913@users.noreply.github.com>
2024-01-24 16:46:38 -0500

    Generic Inspector Base

--- JMayer.Example.WebAssemblyBlazor/JMayer.Example.WebAssemblyBlazor.Client/Pages/Parts/PartInspectorBase.cs
+++ JMayer.Example.WebAssemblyBlazor/JMayer.Example.WebAssemblyBlazor.Client/Pages/Parts/PartInspectorBase.cs
@@ -1,38 +1,12 @@
-﻿using JMayer.Example.WebAssemblyBlazor.Shared.Data.Parts;
+﻿using JMayer.Example.WebAssemblyBlazor.Client.Components.Base;
+using JMayer.Example.WebAssemblyBlazor.Shared.Data.Parts;
 using JMayer.Example.WebAssemblyBlazor.Shared.HTTP.DataLayer.Parts;
-using Microsoft.AspNetCore.Components;
 
 namespace JMayer.Example.WebAssemblyBlazor.Client.Pages.Parts;
 
 /// <summary>
 /// The class manages user interactions with the PartInspector.razor page.
 /// </summary>
-public class PartInspectorBase : ComponentBase
+public class PartInspectorBase : InspectorBase<Part, IPartDataLayer>
 {
-    /// <summary>
-    /// The property gets/sets the data layer to used by the page.
-    /// </summary>
-    [Inject]
-    protected IPartDataLayer DataLayer { get; set; } = null!;
-
-    /// <summary>
-    /// The property gets/sets the index key for the part.
-    /// </summary>
-    [Parameter]
-    public long IndexKey { get; set; }
-
-    /// <summary>
-    /// The property gets/sets the part being inspected.
-    /// </summary>
-    protected Part? Part { get; set; }
-
-    /// <summary>
-    /// The method queries the part based on the set index key.
-    /// </summary>
-    /// <returns>A Task object for the async.</returns>
-    protected override async Task OnParametersSetAsync()
-    {
-        Part = await DataLayer.GetSingleAsync(IndexKey.ToString());
-        await base.OnParametersSetAsync();
-    }
 }
