c3e0e2f43128343121df6c56c7a6f2d1b16c9447
jmayer913 <72579603+jmayer913@users.noreply.github.com>
2024-09-14 13:30:01 -0400

    Other equality comparer unit tests

---
+++ TestProject/Test/EqualityComparer/Parts/PartEqualityComparerUnitTest.cs
@@ -0,0 +1,357 @@
+﻿using JMayer.Example.WebAssemblyBlazor.Shared.Data.Parts;
+
+namespace TestProject.Test.EqualityComparer.Parts;
+
+/// <summary>
+/// The class manages testing the part equality comparer.
+/// </summary>
+public class PartEqualityComparerUnitTest
+{
+    /// <summary>
+    /// The constant for the category.
+    /// </summary>
+    private const string Category = "Category";
+
+    /// <summary>
+    /// The constant for the description.
+    /// </summary>
+    private const string Description = "Description";
+
+    /// <summary>
+    /// The constant for the ID.
+    /// </summary>
+    private const long ID = 1;
+
+    /// <summary>
+    /// The constant for the make.
+    /// </summary>
+    private const string Make = "Make";
+
+    /// <summary>
+    /// The constant for the manufacturer.
+    /// </summary>
+    private const string Manufacturer = "Manufacturer";
+
+    /// <summary>
+    /// The constant for the manufacturer.
+    /// </summary>
+    private const string ManufacturerNumber = "ManufacturerNumber";
+
+    /// <summary>
+    /// The constant for the model.
+    /// </summary>
+    private const string Model = "Model";
+
+    /// <summary>
+    /// The constant for the name.
+    /// </summary>
+    private const string Name = "Name";
+
+    /// <summary>
+    /// The method verifies equality failure when two nulls are compared.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureBothNull() => Assert.False(new PartEqualityComparer().Equals(null, null));
+
+    /// <summary>
+    /// The method verifies equality failure when the Category property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureCategory()
+    {
+        Part part1 = new()
+        {
+            Category = Category,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the CreatedOn property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureCreatedOn()
+    {
+        Part part1 = new()
+        {
+            CreatedOn = DateTime.Now,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the Description property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureDescription()
+    {
+        Part part1 = new()
+        {
+            Description = Description,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the ID property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureID()
+    {
+        Part part1 = new()
+        {
+            Integer64ID = ID,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the LastEditedOn property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureLastEditedOn()
+    {
+        Part part1 = new()
+        {
+            LastEditedOn = DateTime.Now,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the Make property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureMake()
+    {
+        Part part1 = new()
+        {
+            Make = Make,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the Manufacturer property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureManufacturer()
+    {
+        Part part1 = new()
+        {
+            Manufacturer = Manufacturer,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the ManufacturerNumber property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureManufacturerNumber()
+    {
+        Part part1 = new()
+        {
+            ManufacturerNumber = ManufacturerNumber,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the Model property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureModel()
+    {
+        Part part1 = new()
+        {
+            Model = Model,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the Name property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureName()
+    {
+        Part part1 = new()
+        {
+            Name = Name,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the Obsolete property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureObsolete()
+    {
+        Part part1 = new()
+        {
+            Obsolete = true,
+        };
+        Part part2 = new();
+
+        Assert.False(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when an object and null are compared.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureOneIsNull()
+    {
+        Part part = new()
+        {
+            Category = Category,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Make = Make,
+            Manufacturer = Manufacturer,
+            ManufacturerNumber = ManufacturerNumber,
+            Model = Model,
+            Name = Name,
+            Obsolete = true,
+        };
+
+        Assert.False(new PartEqualityComparer().Equals(part, null));
+        Assert.False(new PartEqualityComparer().Equals(null, part));
+    }
+
+    /// <summary>
+    /// The method verifies equality success when two objects (different references) are compared.
+    /// </summary>
+    [Fact]
+    public void VerifySuccess()
+    {
+        Part part1 = new()
+        {
+            Category = Category,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Make = Make,
+            Manufacturer = Manufacturer,
+            ManufacturerNumber = ManufacturerNumber,
+            Model = Model,
+            Name = Name,
+            Obsolete = true,
+        };
+        Part part2 = new(part1);
+
+        Assert.True(new PartEqualityComparer().Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality success when two objects (different references) are compared
+    /// but the LastEditedOn property is excluded from the check.
+    /// </summary>
+    [Fact]
+    public void VerifySuccessExcludeCreatedOn()
+    {
+        Part part1 = new()
+        {
+            Category = Category,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Make = Make,
+            Manufacturer = Manufacturer,
+            ManufacturerNumber = ManufacturerNumber,
+            Model = Model,
+            Name = Name,
+            Obsolete = true,
+        };
+        Part part2 = new(part1)
+        {
+            CreatedOn = DateTime.MinValue,
+        };
+
+        Assert.True(new PartEqualityComparer(true, false, false).Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality success when two objects (different references) are compared
+    /// but the Integer64ID property is excluded from the check.
+    /// </summary>
+    [Fact]
+    public void VerifySuccessExcludeID()
+    {
+        Part part1 = new()
+        {
+            Category = Category,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Make = Make,
+            Manufacturer = Manufacturer,
+            ManufacturerNumber = ManufacturerNumber,
+            Model = Model,
+            Name = Name,
+            Obsolete = true,
+        };
+        Part part2 = new(part1)
+        {
+            Integer64ID = ID + 1,
+        };
+
+        Assert.True(new PartEqualityComparer(false, true, false).Equals(part1, part2));
+    }
+
+    /// <summary>
+    /// The method verifies equality success when two objects (different references) are compared
+    /// but the LastEditedOn property is excluded from the check.
+    /// </summary>
+    [Fact]
+    public void VerifySuccessExcludeLastEditedOn()
+    {
+        Part part1 = new()
+        {
+            Category = Category,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Make = Make,
+            Manufacturer = Manufacturer,
+            ManufacturerNumber = ManufacturerNumber,
+            Model = Model,
+            Name = Name,
+            Obsolete = true,
+        };
+        Part part2 = new(part1)
+        {
+            LastEditedOn = DateTime.MinValue,
+        };
+
+        Assert.True(new PartEqualityComparer(false, false, true).Equals(part1, part2));
+    }
+}
---
+++ TestProject/Test/EqualityComparer/Parts/StockEqualityComparerUnitTest.cs
@@ -0,0 +1,306 @@
+﻿using JMayer.Example.WebAssemblyBlazor.Shared.Data.Parts;
+
+namespace TestProject.Test.EqualityComparer.Parts;
+
+/// <summary>
+/// The class manages testing the stock equality comparer.
+/// </summary>
+public class StockEqualityComparerUnitTest
+{
+    /// <summary>
+    /// The constant for the amount.
+    /// </summary>
+    private const decimal Amount = 100;
+
+    /// <summary>
+    /// The constant for the description.
+    /// </summary>
+    private const string Description = "Description";
+
+    /// <summary>
+    /// The constant for the ID.
+    /// </summary>
+    private const long ID = 1;
+
+    /// <summary>
+    /// The constant for the name.
+    /// </summary>
+    private const string Name = "Name";
+
+    /// <summary>
+    /// The constant for the owner ID.
+    /// </summary>
+    private const long OwnerID = 1;
+
+    /// <summary>
+    /// The constant for the storage location ID.
+    /// </summary>
+    private const long StorageLocationID = 1;
+
+    /// <summary>
+    /// The constant for the storage location name.
+    /// </summary>
+    private const string StorageLocationName = "Storage Location Name";
+
+    /// <summary>
+    /// The method verifies equality failure when the Amount property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureAmount()
+    {
+        Stock stock1 = new()
+        {
+            Amount = Amount,
+        };
+        Stock stock2 = new();
+
+        Assert.False(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the CreatedOn property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureCreatedOn()
+    {
+        Stock stock1 = new()
+        {
+            CreatedOn = DateTime.Now,
+        };
+        Stock stock2 = new();
+
+        Assert.False(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the Description property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureDescription()
+    {
+        Stock stock1 = new()
+        {
+            Description = Description,
+        };
+        Stock stock2 = new();
+
+        Assert.False(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the ID property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureID()
+    {
+        Stock stock1 = new()
+        {
+            Integer64ID = ID,
+        };
+        Stock stock2 = new();
+
+        Assert.False(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the LastEditedOn property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureLastEditedOn()
+    {
+        Stock stock1 = new()
+        {
+            LastEditedOn = DateTime.Now,
+        };
+        Stock stock2 = new();
+
+        Assert.False(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the Name property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureName()
+    {
+        Stock stock1 = new()
+        {
+            Name = Name,
+        };
+        Stock stock2 = new();
+
+        Assert.False(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when an object and null are compared.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureOneIsNull()
+    {
+        Stock stock = new()
+        {
+            Amount = Amount,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Name = Name,
+            OwnerInteger64ID = OwnerID,
+            StorageLocationId = StorageLocationID,
+            StorageLocationName = StorageLocationName,
+        };
+
+        Assert.False(new StockEqualityComparer().Equals(stock, null));
+        Assert.False(new StockEqualityComparer().Equals(null, stock));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the OwnerID property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureOwnerID()
+    {
+        Stock stock1 = new()
+        {
+            OwnerInteger64ID = OwnerID,
+        };
+        Stock stock2 = new();
+
+        Assert.False(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the StorageLocationID property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureStorageLocationID()
+    {
+        Stock stock1 = new()
+        {
+            StorageLocationId = StorageLocationID,
+        };
+        Stock stock2 = new();
+
+        Assert.False(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality failure when the StorageLocationName property is different between the two objects.
+    /// </summary>
+    [Fact]
+    public void VerifyFailureStorageLocationName()
+    {
+        Stock stock1 = new()
+        {
+            StorageLocationName = StorageLocationName,
+        };
+        Stock stock2 = new();
+
+        Assert.False(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality success when two objects (different references) are compared.
+    /// </summary>
+    [Fact]
+    public void VerifySuccess()
+    {
+        Stock stock1 = new()
+        {
+            Amount = Amount,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Name = Name,
+            OwnerInteger64ID = OwnerID,
+            StorageLocationId = StorageLocationID,
+            StorageLocationName = StorageLocationName,
+        };
+        Stock stock2 = new(stock1);
+
+        Assert.True(new StockEqualityComparer().Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality success when two objects (different references) are compared
+    /// but the LastEditedOn property is excluded from the check.
+    /// </summary>
+    [Fact]
+    public void VerifySuccessExcludeCreatedOn()
+    {
+        Stock stock1 = new()
+        {
+            Amount = Amount,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Name = Name,
+            OwnerInteger64ID = OwnerID,
+            StorageLocationId = StorageLocationID,
+            StorageLocationName = StorageLocationName,
+        };
+        Stock stock2 = new(stock1)
+        {
+            CreatedOn = DateTime.MinValue,
+        };
+
+        Assert.True(new StockEqualityComparer(true, false, false).Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality success when two objects (different references) are compared
+    /// but the Integer64ID property is excluded from the check.
+    /// </summary>
+    [Fact]
+    public void VerifySuccessExcludeID()
+    {
+        Stock stock1 = new()
+        {
+            Amount = Amount,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Name = Name,
+            OwnerInteger64ID = OwnerID,
+            StorageLocationId = StorageLocationID,
+            StorageLocationName = StorageLocationName,
+        };
+        Stock stock2 = new(stock1)
+        {
+            Integer64ID = ID + 1,
+        };
+
+        Assert.True(new StockEqualityComparer(false, true, false).Equals(stock1, stock2));
+    }
+
+    /// <summary>
+    /// The method verifies equality success when two objects (different references) are compared
+    /// but the LastEditedOn property is excluded from the check.
+    /// </summary>
+    [Fact]
+    public void VerifySuccessExcludeLastEditedOn()
+    {
+        Stock stock1 = new()
+        {
+            Amount = Amount,
+            CreatedOn = DateTime.Now,
+            Description = Description,
+            Integer64ID = ID,
+            LastEditedOn = DateTime.Now,
+            Name = Name,
+            OwnerInteger64ID = OwnerID,
+            StorageLocationId = StorageLocationID,
+            StorageLocationName = StorageLocationName,
+        };
+        Stock stock2 = new(stock1)
+        {
+            LastEditedOn = DateTime.MinValue,
+        };
+
+        Assert.True(new StockEqualityComparer(false, false, true).Equals(stock1, stock2));
+    }
+}
